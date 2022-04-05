c9b75b3b31241c0d1c2c3fad9bcee30d7b23ead6
Daniel Maldonado <damaldy19@hotmail.com>
2022-04-05 18:50:17 -0500

    Basic algorithm created

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hi!")
+/*
+1.Create the algorithm for applying any discount to any item.
+*/
+
+//1.
+var itemPrice, discountPercent, newItemPrice;
+
+itemPrice = 850;
+discountPercent = 15;
+
+newItemPrice = (itemPrice * (100 - discountPercent)) / 100;
+
+console.log(newItemPrice);
+
+
+
