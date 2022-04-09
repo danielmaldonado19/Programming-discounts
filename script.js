95c84dd34742dd612304cbea79583d47eeb5f3dc
Daniel Maldonado <damaldy19@hotmail.com>
2022-04-08 20:21:21 -0500

    Coupons function created

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 3.Create the input boxes for entering the info for each item.
 4.Create the activation function wich will run the program by clicking the button.
 5.Configure the program for allowing discount cupons by typing codes.
+6.Create a function wich apply to the price after discount, an additional discount for the coupon code.
 */
 
 var coupon = [
@@ -20,17 +21,35 @@ function discount(itemPrice, discountPercent){
     return newItemPrice;      
 }
 
+//6.
+function couponDiscount(priceAfterFirstDiscount){
+    var code = document.getElementById("inputCoupon").value;
+    var priceAfterSecondDiscount;
+    value;
+    if(code == coupon[0]){
+        priceAfterSecondDiscount = priceAfterFirstDiscount * 0.975;       
+    }
+    else if(code == coupon[1]){
+        priceAfterSecondDiscount = priceAfterFirstDiscount * 0.95;       
+    }
+    else if(code == coupon[2]){
+        priceAfterSecondDiscount = priceAfterFirstDiscount * 0.925;       
+    }
+    else{
+        priceAfterSecondDiscount = priceAfterFirstDiscount;
+    }
+}
+
 //4.
 function applyDiscount(){
     var inItemName = document.getElementById("inputItemName").value;
     var inItemPrice = document.getElementById("inputItemPrice").value;
     var inItemDisc = document.getElementById("inputItemDisc").value;
-    var inCoupon = document.getElementById("inputCoupon").value;
 
-    var newPrice = discount(inItemPrice,inItemDisc);
+    var firstDiscountPrice = discount(inItemPrice,inItemDisc);    
 
     document.getElementById("showNewPrice").innerHTML = "The price of " + inItemName + " once the discount of " + inItemDisc + "%" +
-    " was applied is " + newPrice;
+    " was applied is " + firstDiscountPrice;
 
 }
 
